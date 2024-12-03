ddc3b046620b54ad855b4ccaa6130c911ed1c5e3
abinesh-cpu <abineshrl476@gmjail.com>
2024-12-04 00:15:36 +0530

    django works

--- project4/myapp3/urls.py
+++ project4/myapp3/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index),
+    path('',views.userlogin),
     path('adminlogin',views.adminlogin),
     path('adminhome',views.adminhome),
+    path('userregister',views.userreg),
+    path('userhome',views.userhome)
 ]
--- project4/myapp3/views.py
+++ project4/myapp3/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 users=[]
-def index(request):
+def userlogin(request):
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
         users.append({'username':username,'password':password})
-        return redirect(adminlogin)
-    return render(request,'userlogin.html')
+        return redirect(userhome)
+    return render(request,'userlogin.html',{'users':users})
 adminusername="abi123"
 adminpassword="abi@123"
 def adminlogin(request):
@@ -20,5 +20,12 @@ def adminlogin(request):
             return redirect(adminhome)
     return render(request,'adminlogin.html')
 def adminhome(request):
-    return render(request,'adminhome.html',{'users':users})
-    
+    return render(request,'adminhome.html')
+def userreg(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        return redirect(userlogin)
+    return render(request,'userregister.html')
+def userhome(request):
+    return render(request,'userhome.html')
