fac674e88e678397b5d016752d5d198898b217de
abinesh-cpu <abineshrl476@gmjail.com>
2024-12-03 13:21:44 +0530

    django works

--- project4/myapp3/urls.py
+++ project4/myapp3/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index),
-    path('adminlogin',views.adminlogin)
+    path('adminlogin',views.adminlogin),
+    path('adminhome',views.adminhome),
 ]
--- project4/myapp3/views.py
+++ project4/myapp3/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+from django.http import HttpResponse
 # Create your views here.
 users=[]
 def index(request):
@@ -8,6 +8,17 @@ def index(request):
         password=request.POST['password']
         users.append({'username':username,'password':password})
         return redirect(adminlogin)
-    return render(request,'index.html')
+    return render(request,'userlogin.html')
+adminusername="abi123"
+adminpassword="abi@123"
 def adminlogin(request):
-    return render(request,'admin_login.html')
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        if username==adminusername and password==adminpassword:
+            print("logged in")
+            return redirect(adminhome)
+    return render(request,'adminlogin.html')
+def adminhome(request):
+    return render(request,'adminhome.html',{'users':users})
+    
